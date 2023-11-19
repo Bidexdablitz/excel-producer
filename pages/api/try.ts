517b13e252a43ebee145f1329406ab5b553a6530
Bidexdablitz <bamideledamilola3@gmail.com>
2023-11-19 17:03:43 +0100

    new approach

--- pages/api/try.ts
+++ pages/api/try.ts
@@ -5,18 +5,16 @@ type ResponseData = {
     message: string;
 };
 
-type RequestData = {
-    data: string;
+type RequestBody = {
+    file: File;
 };
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
-    const body: RequestData = req.body;
-    const buffer = Buffer.from(body.data, "binary");
-
-    let wb = xlsx.read(buffer);
-    console.log(wb.SheetNames);
+    const { file }: RequestBody = req.body;
+    console.log("file: ", file);
+    let wb = xlsx.read(file.arrayBuffer);
 
     res.status(200).json({ message: "Hello from Next.js!" });
 }
