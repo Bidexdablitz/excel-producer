70e01d539073b1f25d06c834c6b0a0879d052378
Bidexdablitz <bamideledamilola3@gmail.com>
2023-11-23 10:14:26 +0100

    trial

--- pages/api/jsontoexcel.ts
+++ pages/api/jsontoexcel.ts
@@ -3,7 +3,7 @@ var xlsx = require("xlsx");
 
 type ResponseData = {
     json: any;
-    columns: number;
+    tableRange: string;
 };
 
 type RequestBody = {
@@ -24,9 +24,36 @@ export default function handler(
     xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
     // generate a bytes file to return to the user
     let file = xlsx.write(wb, { bookType: "xlsx", type: "base64" });
+    const lenOfColumns = Object.keys(transactions[0]).length;
+    let tableRange = `A1:${calcBottomRange(lenOfColumns)}${calcBottomRange(
+        lenOfColumns
+    )}`;
+    console.log("table range is: ", tableRange);
+    res.status(200).json({ json: file, tableRange });
+}
 
-    res.status(200).json({
-        json: file,
-        columns: Object.keys(transactions[0]).length,
-    });
+function calcBottomRange(numOfColumns: number) {
+    // create list of alphabeth
+    const alpha = Array.from(Array(27)).map((e, i) => i + 64);
+    const alphabet = alpha.map((x) => String.fromCharCode(x));
+    let text = "";
+
+    return (function calculateText(number) {
+        if (number < 27) {
+            text += alphabet[number];
+        } else {
+            let divided = Math.floor(number / 26);
+            let remainder = number % 26;
+            if (remainder === 0) {
+                if (divided > 26) calculateText(divided - 1);
+                else text += alphabet[divided - 1];
+                text += alphabet[26];
+            } else {
+                if (divided > 26) calculateText(divided);
+                else text += alphabet[divided];
+                text += alphabet[remainder];
+            }
+        }
+        return text;
+    })(numOfColumns);
 }
